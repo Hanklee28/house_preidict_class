db76b02316f1ffb0e6abcb595c773095eeac1084
tazz7420 <xinzhe458@gmail.com>
2022-09-12 10:54:26 +0800

    20220912update

--- app.py
+++ app.py
@@ -58,15 +58,49 @@ def get_form():
 
         # 經濟指標
         target_layer = gpd.read_file('./layer/economic_indicators/conveniencestore.geojson', encoding = 'utf-8')
-        conveniencestore = house.sjoin_point_layer(target_layer, 'conveniencestore', '分公司名稱', 'count')
+        conveniencestore = house.sjoin_point_layer(target_layer, 'conveniencestore_count', '分公司名稱', 'count')
         target_layer = gpd.read_file('./layer/economic_indicators/fastfood.geojson', encoding = 'utf-8')
-        fastfood = house.overlay_polygon_layer(target_layer, 'fastfood', 'full_id', 'count')
-        print(fastfood)
+        fastfood = house.overlay_polygon_layer(target_layer, 'fastfood_count', 'full_id', 'count')
 
         # 文教機構
-        # target_layer = gpd.read_file('./layer/educational_resources/library.geojson', encoding = 'utf-8')
-        # library = house.overlay_polygon_layer(target_layer, 'library', 'full_id', 'count')
+        target_layer = gpd.read_file('./layer/educational_resources/library.geojson', encoding = 'utf-8')
+        library = house.overlay_polygon_layer(target_layer, 'library_count', 'full_id', 'count')
+        target_layer = gpd.read_file('./layer/educational_resources/school.geojson', encoding = 'utf-8')
+        school = house.overlay_polygon_layer(target_layer, 'near_school', 'full_id', 'near')
+        target_layer = gpd.read_file('./layer/educational_resources/university.geojson', encoding = 'utf-8')
+        university = house.overlay_polygon_layer(target_layer, 'near_university', 'full_id', 'near')
 
+        # 公共安全
+        target_layer = gpd.read_file('./layer/public_safety/firestation.geojson', encoding = 'utf-8')
+        firestation = house.sjoin_point_layer(target_layer, 'near_firestation', '消防隊名稱', 'near')
+        target_layer = gpd.read_file('./layer/public_safety/fuel.geojson', encoding = 'utf-8')
+        fuel = house.overlay_polygon_layer(target_layer, 'fuel_count', 'full_id', 'count')
+        target_layer = gpd.read_file('./layer/public_safety/market.geojson', encoding = 'utf-8')
+        market = house.overlay_polygon_layer(target_layer, 'market_count', 'full_id', 'count')
+        target_layer = gpd.read_file('./layer/public_safety/police.geojson', encoding = 'utf-8')
+        police = house.sjoin_point_layer(target_layer, 'near_police', '中文單位名稱', 'near')
+        target_layer = gpd.read_file('./layer/public_safety/placeofworkship.geojson', encoding = 'utf-8')
+        placeofworkship = house.overlay_polygon_layer(target_layer, 'placeofworkship_count', 'full_id', 'count')
+
+        # 自然環境
+        target_layer = gpd.read_file('./layer/natural_environment/cemetery.geojson', encoding = 'utf-8')
+        cemetery = house.overlay_polygon_layer(target_layer, 'cemetery_area', 'full_id', 'area')
+        target_layer = gpd.read_file('./layer/natural_environment/park.geojson', encoding = 'utf-8')
+        park = house.overlay_polygon_layer(target_layer, 'park_area', 'full_id', 'area')
+        target_layer = gpd.read_file('./layer/natural_environment/river_TW.geojson', encoding = 'utf-8')
+        river_TW = house.overlay_polygon_layer(target_layer, 'river_TW_area', 'full_id', 'area')
+
+        # 交通運輸
+        target_layer = gpd.read_file('./layer/transportation/parking.geojson', encoding = 'utf-8')
+        parking = house.overlay_polygon_layer(target_layer, 'parking_area', 'full_id', 'area')
+        target_layer = gpd.read_file('./layer/transportation/busstop.geojson', encoding = 'utf-8')
+        busstop = house.sjoin_point_layer(target_layer, 'busstop_count', 'full_id', 'count')
+        target_layer = gpd.read_file('./layer/transportation/LRT.geojson', encoding = 'utf-8')
+        LRT = house.sjoin_point_layer(target_layer, 'near_LRT', 'MARKID', 'near')
+        target_layer = gpd.read_file('./layer/transportation/MRT.geojson', encoding = 'utf-8')
+        MRT = house.sjoin_point_layer(target_layer, 'near_MRT', 'MARKID', 'near')
+        target_layer = gpd.read_file('./layer/transportation/TRA.geojson', encoding = 'utf-8')
+        TRA = house.sjoin_point_layer(target_layer, 'near_TRA', 'MARKID', 'near')
 
         result = house.return_geo_dataframe()
         result.to_csv('codeCheckFile.csv')
